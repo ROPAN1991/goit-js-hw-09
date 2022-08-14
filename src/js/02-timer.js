1f195a10ea7c445b48b5385f8440cc47aaa95a16
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-08-14 10:33:29 +0300

    hwgoit09

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,5 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-
 const input = document.getElementById('datetime-picker');
 const button = document.querySelector('[data-start]');
 const days = document.querySelector('[data-days]');
@@ -8,7 +7,7 @@ const hours = document.querySelector('[data-hours]');
 const minutes = document.querySelector('[data-minutes]');
 const seconds = document.querySelector('[data-seconds]');
 let interval;
-
+button.disabled = true;
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
@@ -20,21 +19,18 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   return { days, hours, minutes, seconds };
 }
-
 function addLeadingZero(value) {
   if (String(value).length === 1) {
     return '0' + value;
   }
   return value;
 }
-
 function createNotification() {
   const div = document.createElement('div');
   div.classList.add('notification');
   div.innerHTML = 'Please choose a date in the future';
   return div;
 }
-
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -47,39 +43,28 @@ const options = {
       setTimeout(() => {
         document.querySelector('.notification').remove();
       }, 4000);
+      return;
     }
-  },
-  onChange([selectedDate], _, instance) {
-    const date = new Date();
-    if (selectedDate.getTime() <= date.getTime()) {
-      button.disabled = true;
-      instance.input.disabled = true;
-    } else {
-      button.disabled = false;
-      instance.input.disabled = false;
-    }
+    button.disabled = false;
   },
 };
-
 flatpickr(input, options);
-
 button.addEventListener('click', () => {
-  clearInterval(interval);
+  input.disabled = true;
   const inputValue = input.value;
   interval = setInterval(() => {
     const currentMs = new Date().getTime();
     const selectedMs = Date.parse(inputValue);
     const ms = selectedMs - currentMs;
     const values = convertMs(ms);
-
-    if (ms < 1000) {
-      return clearInterval(interval);
-    }
-
     days.innerHTML = addLeadingZero(values.days);
     hours.innerHTML = addLeadingZero(values.hours);
     minutes.innerHTML = addLeadingZero(values.minutes);
     seconds.innerHTML = addLeadingZero(values.seconds);
+    if (ms < 1000) {
+      input.disabled = false;
+      return clearInterval(interval);
+    }
   }, 1000);
   button.disabled = true;
 });
